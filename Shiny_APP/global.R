c431073dfa20f6f8777883b8db801e569b17be78
Mario S <35705112+mhsmario@users.noreply.github.com>
2023-01-22 23:22:41 -0500

    Ready for the public
    Removed sensitive data.

--- Shiny_APP/global.R
+++ Shiny_APP/global.R
@@ -25,16 +25,7 @@ library(shinyFiles)
 library(glue)
 library(tidyverse)
 #####0.Libraries####
-#source("src/0. Preparando o RStudio.R")
-#source("src/1.Assign_ID_New_Form_Entries.R")
-# source("src/2.Send_to_Triagem.R")
-# source("src/3.update_prayer_requests.R")
-# source("src/4.Insert_IDs_Preencher_Transporte.R")
-# source("src/6.Update_Contact_Info.R")
-# source("src/7.Prepare_names_for_python.R")
-# source("src/8.Drivers_sheet.R")
-# source("src/9.Update_Delivered.R")
-# source("src/11.Update_Tableau_Map.R")
+
 
 #Set up Credentials -------
 # designate project-specific cache
@@ -43,13 +34,13 @@ options(gargle_oauth_cache = ".secrets")
 gargle::gargle_oauth_cache()
 # # trigger auth on purpose to store a token in the specified cache
 # # a broswer will be opened
-#googlesheets4::sheets_auth(email = "iirbrasilsocial@gmail.com")
+
 # # see your token file in the cache, if you like
 #list.files(".secrets/")
 
 googlesheets4::gs4_auth(
   cache = ".secrets",
-  email = "iirbrasilsocial@gmail.com"
+  email = "[...]@gmail.com"
 )
 
 
